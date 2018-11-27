494a025d0c2c5d9fc3ca9e34b7408a836faca8a0
Jan Růžička <honza@Jans-MacBook-Pro.local>
2018-11-27 14:27:41 +0100

    Login Completed Style

--- src/scripts/parts/AdminLogin.tsx
+++ src/scripts/parts/AdminLogin.tsx
@@ -1,29 +1,28 @@
 import * as React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 import FormControl from '@material-ui/core/FormControl';
 import InputAdornment from '@material-ui/core/InputAdornment';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 
-const styles = {
-  root: {
-    display: 'flex',
-  },
-  textField: {
-    flexBasis: 200,
-  },
-};
-
-
-class AdminLogin extends React.Component {
+export default class AdminLogin extends React.Component {
 	props: any;
 
   state = {
@@ -33,41 +32,72 @@ class AdminLogin extends React.Component {
 
 
   handleChangePassword = (event: any) => {
-    this.state.password = event.target.value;
+    this.setState({ password: event.target.value });
   };
 
   handleClickShowPassword = () => {
-    this.state.showPassword = !this.state.showPassword;
+    this.setState({ showPassword: !this.state.showPassword });
   };
 
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={classes.root}>
-        <FormControl className={classes.textField}>
-          <InputLabel htmlFor="passwordField">Heslo</InputLabel>
-          <Input
-            id="passwordField"
-            type={this.state.showPassword ? 'text' : 'password'}
-            value={this.state.password}
-            onChange={this.handleChangePassword}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="Přepnout viditelnost hesla"
-                  onClick={this.handleClickShowPassword}
-                >
-                  {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-      </div>
+      <Grid
+        container
+        spacing={24}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item xs={12}></Grid>
+
+        <Grid item sm={12}>
+          <Paper>
+            <Grid
+              container
+              spacing={24}
+              direction="column"
+              alignItems="center"
+              justify="center"
+
+              className="layout-content-grid"
+            >
+              <Grid item xs="auto">
+                  <Typography variant="title" color="inherit" className="layout-title">
+                    Administrátor - Přihlášení
+                  </Typography>
+              </Grid>
+
+              <Grid item xs="auto">
+                <FormControl className="text-field">
+                  <InputLabel htmlFor="passwordField">Heslo</InputLabel>
+                  <Input
+                    id="passwordField"
+                    type={this.state.showPassword ? 'text' : 'password'}
+                    value={this.state.password}
+                    onChange={this.handleChangePassword}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Přepnout viditelnost hesla"
+                          onClick={this.handleClickShowPassword}
+                        >
+                          {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+
+              <Grid item xs="auto">
+                <Button variant="contained" className="layout-button">
+                  Přihlásit
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     );
   }
 }
-
-
-export default withStyles(styles)(AdminLogin);
