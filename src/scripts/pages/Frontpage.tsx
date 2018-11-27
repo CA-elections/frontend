581b05b83a42578be4e988aac5f8f8071f82b435
Krystof Mitka <krystof.mitka@seznam.cz>
2018-11-27 15:47:22 +0100

    Frontpage: tweaks

--- src/scripts/pages/Frontpage.tsx
+++ src/scripts/pages/Frontpage.tsx
@@ -5,21 +5,29 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Icon from '@material-ui/core/Icon';
+import classNames from 'classnames'
+import Button from "@material-ui/core/es/Button/Button";
 
 const styles = (theme: any) => ({
 	root: {
 		width: '80%',
 		margin: '50px auto 0'
 	},
+	panel: {
+		'border-left': '3px solid red'
+	},
 	heading: {
 		fontSize: theme.typography.pxToRem(18),
 		fontWeight: theme.typography.fontWeightRegular,
 		flexShrink: 0,
+		'margin-left': '5px'
 	},
 	secondaryHeading: {
 		fontSize: theme.typography.pxToRem(15),
 		color: theme.palette.text.secondary,
-		'margin-left': 'auto'
+		'margin-left': 'auto',
+		'align-self': 'center'
 	}
 });
 
@@ -27,8 +35,9 @@ function Frontpage(props: any) {
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<ExpansionPanel>
+			<ExpansionPanel className={classNames('panel-expansion','view-red')}>
 				<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+					<Icon>account_balance</Icon>
 					<Typography className={classes.heading}>Election for the new president</Typography>
 					<Typography className={classes.secondaryHeading}>
 						30.11.2018 - 05.12.2018
@@ -39,20 +48,27 @@ function Frontpage(props: any) {
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
 						sit amet blandit leo lobortis eget.
 					</Typography>
+					<Button variant="contained" color="primary" >
+						Primary
+					</Button>
 				</ExpansionPanelDetails>
 			</ExpansionPanel>
-			<ExpansionPanel>
+			<ExpansionPanel className={classNames('panel-expansion','view-green')}>
 				<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+					<Icon>account_balance</Icon>
 					<Typography className={classes.heading}>Election to the EU parlamentt</Typography>
 					<Typography className={classes.secondaryHeading}>
 						20.11.2018 - 27.11.2018
 					</Typography>
 				</ExpansionPanelSummary>
-				<ExpansionPanelDetails>
+				<ExpansionPanelDetails className={classNames('panel-asd'}>
 					<Typography>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
 						sit amet blandit leo lobortis eget.
 					</Typography>
+					<Button variant="contained" color="primary" >
+						Primary
+					</Button>
 				</ExpansionPanelDetails>
 			</ExpansionPanel>
 		</div>
