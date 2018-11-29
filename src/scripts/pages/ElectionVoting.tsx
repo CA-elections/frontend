3ee1ffbcedc0dae4b24038839f6c8879f6ba5ea7
Krystof Mitka <krystof.mitka@seznam.cz>
2018-11-29 18:08:26 +0100

    ElectionVoting: nearly done

--- src/scripts/pages/ElectionVoting.tsx
+++ src/scripts/pages/ElectionVoting.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { withStyles } from '@material-ui/core/styles';
 import Layout from "../layout";
 
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -7,6 +6,8 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from "@material-ui/core/typography";
+import Grid from "@material-ui/core/Grid/Grid";
+import Button from "@material-ui/core/Button/Button";
 
 interface ElectionVotingProps {
 	match: any
@@ -14,42 +15,103 @@ interface ElectionVotingProps {
 
 export default class ElectionVoting extends React.Component<ElectionVotingProps> {
 	state = {
-		election: {
-			name: "Hlasování",
-			desc: "Tohle je to nejlepší hlasování ever."
-		},
-		candidates: [],
+		votes_available: 0,
+		election_id: 1,
+		candidates: Array(),
+		name: "",
+		desc: "",
+		votes: Array()
 	};
 
 	constructor(props: ElectionVotingProps) {
 		super(props);
 
-		this.fetchElectionData()
+		this.fetchCodeData()
+		this.addVote = this.addVote.bind(this)
+		this.removeVote = this.removeVote.bind(this)
 	}
 
-	fetchElectionData() {
-		fetch("http://hmmmm.magnusi.tech/api/notification/" + this.props.match.params.token)
+	fetchCodeData() {
+		const baseUrl = "http://hmmmm.magnusi.tech/api";
+		fetch(baseUrl + "/notification/" + this.props.match.params.token)
 			.then(result => {
 				return result.json();
 			})
 			.then(result => {
 				this.setState(result)
-				console.log(this.state);
+
+				fetch(baseUrl + "/election/" + this.state.election_id)
+					.then(result => {
+						return result.json();
+					})
+					.then(result => {
+						this.setState(result)
+						console.log(this.state);
+					})
+			}).catch((e) => {
+				console.log(e)
 			})
+	}
+
+	addVote(id: number) {
+		this.state.votes.push(id)
+
+		this.setState({ votes: this.state.votes } );
+		console.log(this.state.votes)
+	}
+
+	removeVote(id: number) {
+		const result = this.state.votes.filter((x) => x != id);
+		console.log(result)
+
+		this.setState({ votes: result })
+		console.log(this.state.votes)
 	}
 
 	render() {
 		return (
-			<Layout title={this.state.election.name} desc={this.state.election.desc}>
+			<Layout title={this.state.name} desc={this.state.desc}>
+				<h2 className="layout-votes">Hlasů: {this.state.votes_available - this.state.votes.length}</h2>
+
 				{this.state.candidates.map((value: any, index: any) => (
 					<ExpansionPanel key={index}>
 						<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
 							<Typography>{value.name} {value.surname}</Typography>
 						</ExpansionPanelSummary>
 						<ExpansionPanelDetails>
-							<Typography>
-								{value.annotation}
-							</Typography>
+							<Grid
+								container
+								spacing={24}
+								direction="column"
+								alignItems="center"
+								justify="center"
+
+							>
+								<Typography>
+									{value.annotation} Prosím tohle potom smazat, je to tu jenom protože sem nidko nedal žádnou anotaci
+								</Typography>
+								<div className="layout-button-wrapper">
+									<Button
+										disabled={this.state.votes.indexOf(value.id) === -1}
+										variant="contained"
+										color="secondary"
+										className="layout-button-btn"
+										onClick={() => this.removeVote(value.id)}
+									>
+										Odebrat hlas
+									</Button>
+
+									<Button
+										disabled={this.state.votes_available === this.state.votes.length}
+										variant="contained"
+										color="primary"
+										className="layout-button-btn"
+										onClick={() => this.addVote(value.id)}
+									>
+										Přidat hlas
+									</Button>
+								</div>
+							</Grid>
 						</ExpansionPanelDetails>
 					</ExpansionPanel>
 				)}
